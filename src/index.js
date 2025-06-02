f9a8d62a35c8aaa6511b2c6737633cffe047d770
YUVALF14 <yuvalf14@gmail.com>
2025-06-02 23:56:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 // Import fonts
 // import '@fontsource/assistant/500.css';
 // import '@fontsource/assistant/600.css';
