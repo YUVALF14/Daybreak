c76f68487311fc8e9e3e4ca6bf88c4103f5be17f
YUVALF14 <yuvalf14@gmail.com>
2025-06-03 10:51:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+
 // Import fonts
-// import '@fontsource/assistant/500.css';
-// import '@fontsource/assistant/600.css';
-// import '@fontsource/assistant/700.css';
+import '@fontsource/assistant/400.css';
+import '@fontsource/assistant/500.css';
+import '@fontsource/assistant/600.css';
+import '@fontsource/assistant/700.css';
+import '@fontsource/heebo/400.css';
+import '@fontsource/heebo/500.css';
+import '@fontsource/heebo/700.css';
+import '@fontsource/heebo/800.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
