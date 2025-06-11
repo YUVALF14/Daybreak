3859b4fc3e3e81b6d81f52b7dc991f6bc43a08cb
YUVALF14 <yuvalf14@gmail.com>
2025-06-12 01:29:51 +0200

    code 100

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,10 @@
-// Firebase configuration
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
-
 const firebaseConfig = {
   apiKey: "AIzaSyCl_KY51O2W5koBIZf9Pdp1GYiqeIPZz3I",
   authDomain: "daybreak-3979a.firebaseapp.com",
   databaseURL: "https://daybreak-3979a.firebaseio.com",
   projectId: "daybreak-3979a",
-  storageBucket: "daybreak-3979a.appspot.com",
+  storageBucket: "daybreak-3979a.firebasestorage.app",
   messagingSenderId: "1065548372608",
   appId: "1:1065548372608:web:487d243faf9b0d89bee465",
   measurementId: "G-B7FYNWLX6V"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app);
-
-export { database, auth };
