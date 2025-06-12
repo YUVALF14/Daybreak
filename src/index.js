3658ce27c553fa932c2d68080cfbc613155da87f
YUVALF14 <yuvalf14@gmail.com>
2025-06-12 19:02:51 +0200

    FINAL?

--- src/index.js
+++ src/index.js
@@ -14,12 +14,22 @@ import '@fontsource/heebo/700.css';
 import '@fontsource/heebo/800.css';
 
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles/theme';
+import { EventsProvider } from './context/EventsContext';
+import { WhatsAppProvider } from './context/WhatsAppContext';
 
 document.documentElement.setAttribute('dir', 'rtl');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <WhatsAppProvider>
+        <EventsProvider>
+          <App />
+        </EventsProvider>
+      </WhatsAppProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
