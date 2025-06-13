fb760bdf9c93c73c9a9f3e0e8bd161d061c033fa
YUVALF14 <yuvalf14@gmail.com>
2025-06-13 14:15:33 +0200

    hello world

--- src/components/EventDashboard.js
+++ src/components/EventDashboard.js
@@ -25,7 +25,7 @@ import {
 import EventForm from './EventForm';
 import ParticipantDialog from './ParticipantDialog';
 import NewEventForm from './NewEventForm';
-import BudgetDashboard from './BudgetDashboard';
+import BudgetDashboard from './BudgetDashboard.js';
 import { useEvents } from '../context/EventsContext';
 
 function EventDashboard() {
